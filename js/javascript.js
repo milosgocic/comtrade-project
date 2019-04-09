069839eec3b1a4735b5b5a78c02c40461bdfeb4f
milosgocic <milosgocic@gmail.com>
2019-04-10 00:53:56 +0200

    sticky

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,6 @@
+
+/*** buttons ***/
+
 $("#btn1").click(function() {
     $('html,body').animate({
         scrollTop: $("#theater").offset().top},
@@ -14,6 +17,8 @@ $("#btn3").click(function() {
         'slow', 'swing');
 });
 
+/*** animations ***/
+
 $(".js--wp1").waypoint(function(direction) {
     $(".js--wp1").addClass("animated pulse");
 }, {
@@ -22,12 +27,12 @@ $(".js--wp1").waypoint(function(direction) {
 $(".js--wp2").waypoint(function(direction) {
     $(".js--wp2").addClass("animated fadeInLeft");
 }, {
- offset: "70%;"
+ offset: "60%;"
 });
 $(".js--wp3").waypoint(function(direction) {
     $(".js--wp3").addClass("animated fadeInUp");
 }, {
- offset: "65%;"
+ offset: "60%;"
 });
 $(".js--wp4").waypoint(function(direction) {
     $(".js--wp4").addClass("animated fadeInRight");
@@ -42,7 +47,7 @@ $(".js--wp5").waypoint(function(direction) {
 $(".js--wp6").waypoint(function(direction) {
     $(".js--wp6").addClass("animated fadeInRight");
 }, {
- offset: "70%;"
+ offset: "55%;"
 });
 $(".js--wp7").waypoint(function(direction) {
     $(".js--wp7").addClass("animated pulse");
@@ -57,50 +62,63 @@ $(".js--wp8").waypoint(function(direction) {
 $(".js--wp9").waypoint(function(direction) {
     $(".js--wp9").addClass("animated zoomIn");
 }, {
- offset: "80%;"
+ offset: "60%;"
 });
 $(".js--wp10").waypoint(function(direction) {
     $(".js--wp10").addClass("animated zoomIn");
 }, {
- offset: "80%;"
+ offset: "60%;"
 });
 $(".js--wp11").waypoint(function(direction) {
     $(".js--wp11").addClass("animated fadeInUp");
 }, {
- offset: "80%;"
+ offset: "60%;"
 });
 $(".js--wp12").waypoint(function(direction) {
     $(".js--wp12").addClass("animated fadeInDown");
 }, {
- offset: "75%;"
+ offset: "60%;"
 });
 $(".js--wp13").waypoint(function(direction) {
     $(".js--wp13").addClass("animated fadeInUp");
 }, {
- offset: "70%;"
+ offset: "60%;"
 });
 $(".js--wp14").waypoint(function(direction) {
     $(".js--wp14").addClass("animated fadeInDown");
 }, {
- offset: "65%;"
+ offset: "60%;"
 });
 $(".js--wp15").waypoint(function(direction) {
     $(".js--wp15").addClass("animated bounceInLeft");
 }, {
- offset: "80%;"
+ offset: "60%;"
 });
 $(".js--wp16").waypoint(function(direction) {
     $(".js--wp16").addClass("animated fadeInLeft");
 }, {
- offset: "75%;"
+ offset: "60%;"
 });
 $(".js--wp17").waypoint(function(direction) {
     $(".js--wp17").addClass("animated zoomInLeft");
 }, {
- offset: "70%;"
+ offset: "60%;"
 });
 $(".js--wp18").waypoint(function(direction) {
     $(".js--wp18").addClass("animated rollIn");
 }, {
- offset: "65%;"
+ offset: "60%;"
 });
+
+/*** sticky ***/
+   
+$(".js-nav").waypoint(function(direction) {
+        if (direction == "down") {
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+        
+        }, {
+        offset: "140px;"
+    });
